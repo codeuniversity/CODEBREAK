97863a7235c339b7765e5125e3ba53c29efbf95d
Lukas Vollmer <github@lukasvollmer.com>
2017-10-27 12:02:06 +0200

    add login to frontend

--- app/src/components/sites/Restaurants.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>Hallo Restaurants</div>
-</template>
-<script></script>
-<style scoped></style>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,27 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/sites/HelloWorld'
-import Restaurants from '@/components/sites/Restaurants'
+import Dashboard from '@/components/sites/Dashboard'
+import Login from '@/components/sites/Login'
 
 Vue.use(Router)
 
-export default new Router({
+var Auth = {
+    loggedIn: false
+};
+
+var router = new Router({
   mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Dashboard',
+      component: Dashboard,
+      meta: { requiresAuth: true }
     },
     {
-        path: '/test',
-        name: 'HelloWorld',
-        component: HelloWorld
-    },
-    {
-      path: '/restaurants',
-      name: 'Restaurants',
-      component: Restaurants
+        path: '/login',
+        name: 'Login',
+        component: Login
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth) && !Auth.loggedIn) {
+        next({ path: '/login', });
+    } else {
+        next();
+    }
+});
+
+
+export default router
